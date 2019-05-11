a19d6b4cb5a9f0a40fa843b1d41e129887f9b5eb
unknown <salayidin103@gmail.com>
2019-05-10 22:14:09 -0400

    New Poolling with realtion matrix

--- Polling.py
+++ Polling.py
@@ -7,7 +7,7 @@ im = cv2.imread('Lenna_test.png')
 cv2.imshow('orginal_img',im)
 im_width = im.shape[0]
 im_height = im.shape[1]
-P_mode = 'max'
+P_mode = 'relation_eigen'
 new_im = Poolling(im, im_width,im_height,4,4,P_mode)
 
 cv2.imshow('new_img',new_im)
@@ -17,5 +17,7 @@ elif P_mode == 'max':
     cv2.imwrite('result_img/max_value.png', new_im)
 elif P_mode =='eigen':
     cv2.imwrite('result_img/max_eigen_value.png', new_im)
-
+elif P_mode =='relation_eigen':
+    cv2.imwrite('result_img/relation_eigen_value_secound.png', new_im)
+    pass
 cv2.waitKey(0)
--- poolling_functions.py
+++ poolling_functions.py
@@ -26,7 +26,11 @@ def Poolling(img, width, height, Polling_w, Polling_h , P_mode):
                     temp = Matrix_Eigen(R_matrix, G_matrix, B_matrix)
                 if P_mode == 'ave':
                     temp = Matrix_Average(R_matrix, G_matrix, B_matrix)
-
+                if P_mode =='relation_eigen':
+                    R_relation_matrix = Matrix_absolute_Relation(R_matrix)
+                    G_relation_matrix = Matrix_absolute_Relation(G_matrix)
+                    B_relation_matrix = Matrix_absolute_Relation(B_matrix)
+                    temp = Matrix_Eigen(R_relation_matrix,G_relation_matrix,B_relation_matrix)
 
                 for ph in range(0,Polling_h):
                     for pw in range(0, Polling_w):
@@ -35,7 +39,8 @@ def Poolling(img, width, height, Polling_w, Polling_h , P_mode):
                 pass
     return img
 
-def Matrix_Eigen(R_matrix, G_matrix, B_matrix):
+
+def Matrix_Eigen(R_matrix, G_matrix, B_matrix): #return RGB_matrix or array of the max element
     R_e_vals, R_e_vecs = LA.eig(R_matrix)
     G_e_vals, G_e_vecs = LA.eig(G_matrix)
     B_e_vals, B_e_vecs = LA.eig(B_matrix)
@@ -67,7 +72,7 @@ def Matrix_Max(R_matrix, G_matrix, B_matrix):
     return RGB_matrix
 
 
-def Matrix_Average(R_matrix, G_matrix, B_matrix):
+def Matrix_Average(R_matrix, G_matrix, B_matrix):#find the average value of the matrix and round it
 
     RGB_matrix = [0, 0, 0]
     RGB_matrix[0] = round(np.average(R_matrix))
@@ -75,3 +80,23 @@ def Matrix_Average(R_matrix, G_matrix, B_matrix):
     RGB_matrix[2] = round(np.average(B_matrix))
 
     return RGB_matrix
+
+
+def Matrix_absolute_Relation(R_matrix): # find the relation matrix of the given matrix
+    R_arr_abso = []
+    for row1 in R_matrix:
+        for col1 in row1:
+            for row2 in R_matrix:
+                for col2 in row2:
+                    if col2 - col1 < 0:
+                        R_arr_abso.append(-1*(col2-col1))
+                    else:
+                        R_arr_abso.append(col2-col1)
+    R_matrix_abso =[]
+    for row in range(0,len(R_arr_abso),len(R_matrix)*len(R_matrix[0])):
+        s=[]
+        for cols in range(len(R_matrix)*len(R_matrix[0])):
+            s.append(R_arr_abso[cols+row])
+        R_matrix_abso.append(s)
+
+    return R_matrix_abso
